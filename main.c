ff15fa8092bf67bfa393baca55529df4f66e3f99
Felipe Silva <felipe.profissionalti@gmail.com>
2021-07-05 19:47:19 -0300

    Corrigidos pequenos erros

--- main.c
+++ main.c
@@ -1,11 +1,4 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-******************************************************************************  
+  
 /*
 Preencha um algoritmo que leia 10 números e os coloque em um vetor.
 Ao final, apresente na tela os valores armazenados nas posições pares
